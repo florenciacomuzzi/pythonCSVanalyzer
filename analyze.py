adddd6b4268dcd87c452d63c7987534caf3cda07
Flo Sal <florenciabsalinas@gmail.com>
2016-04-18 20:52:02 -0400

    first commit

---
+++ analyze.py
@@ -0,0 +1,15 @@
+import csv
+
+def main():
+    agency_counter = collections.Counter()
+    with open('data.csv') as fp:
+        reader = csv.DictReader(fp)
+        for line in reader: 
+            agency = line.get('Agency Name')
+            agency_counter[agency] += 1
+
+    for agency, count in agency_counter.most_common():
+        print(agency, count)
+
+
+main()    
