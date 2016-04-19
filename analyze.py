693a4ce5dac66d3c36157d6deca22eee0d548075
Flo Sal <florenciabsalinas@gmail.com>
2016-04-18 21:09:57 -0400

    add noise percentage

--- analyze.py
+++ analyze.py
@@ -6,14 +6,25 @@ def main():
     complaint_counter = collections.Counter()
     with open('data.csv') as fp:
         reader = csv.DictReader(fp)
-        for line in reader: 
+        noise_counter = 0
+        total = 0
+        for line in reader:
+            total += 1 
             agency = line.get('Agency Name')
             agency_counter[agency] += 1
             complaint = line.get('Complaint Type')
             complaint_counter[complaint] += 1
 
+    
+    for complaint, count in complaint_counter.most_common():
+        if 'noise' in complaint.lower():
+            noise_counter += 1
+        complaint_counter[complaint] += 1
+       
     for complaint, count in complaint_counter.most_common():
         print(complaint, count)
-
+    print()
+    print('Noise Complaints: ', noise_counter)
+    print('Noise %', (noise_counter / total) * 100)
 
 main()    
