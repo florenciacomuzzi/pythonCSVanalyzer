bd59df533f61f3b0e7aecbc4fef92aa016105a94
Flo Sal <florenciabsalinas@gmail.com>
2016-04-18 20:59:56 -0400

    Display and count complaint types

--- analyze.py
+++ analyze.py
@@ -3,14 +3,17 @@ import csv
 
 def main():
     agency_counter = collections.Counter()
+    complaint_counter = collections.Counter()
     with open('data.csv') as fp:
         reader = csv.DictReader(fp)
         for line in reader: 
             agency = line.get('Agency Name')
             agency_counter[agency] += 1
+            complaint = line.get('Complaint Type')
+            complaint_counter[complaint] += 1
 
-    for agency, count in agency_counter.most_common():
-        print(agency, count)
+    for complaint, count in complaint_counter.most_common():
+        print(complaint, count)
 
 
 main()    
